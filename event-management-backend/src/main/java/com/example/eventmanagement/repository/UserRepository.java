71a5cba01b204f6250d7284174334e8d750637e3
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2026-04-23 13:10:39 +0530

    Feat: User Repository

--- event-management-backend/src/main/java/com/example/eventmanagement/repository/UserRepository.java
+++ event-management-backend/src/main/java/com/example/eventmanagement/repository/UserRepository.java
@@ -1,14 +1,9 @@
 package com.example.eventmanagement.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.example.eventmanagement.model.User;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-    long countByRole(UserRole role);
 }
